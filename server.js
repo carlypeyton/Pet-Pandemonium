628351cfc6644c1c732f16f4532cb1c116580972
Carly Peyton <carlypeyton@gmail.com>
2021-04-28 19:59:29 -0400

    Create react app and add passport code.

---
+++ server.js
@@ -0,0 +1,36 @@
+// Requiring necessary npm packages
+const express = require("express");
+const session = require("express-session");
+// Requiring passport as we've configured it
+const passport = require("./config/passport");
+const mongoose = require("mongoose");
+
+// Setting up port and requiring models for syncing
+const PORT = process.env.PORT || 8080;
+
+// Creating express app and configuring middleware needed for authentication
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+// We need to use sessions to keep track of our user's login status
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Requiring our routes
+require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+app.listen(PORT, () => {
+  console.log("App running on Port" + PORT);
+});
