b777fc886b7f91e1e9ccc1c3d10967d318f36475
Carly Peyton <carlypeyton@gmail.com>
2021-05-01 12:07:38 -0400

    Build out front end react components.

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
+import Login from "./pages/Login";
+import Lobby from "./pages/Lobby";
+import Pregame from "./pages/Pregame";
+import Game from "./pages/Game";
+import Postgame from "./pages/Postgame";
+
+
+function App() {
+  return (
+    <Router>
+    <div>
+      <NavBar />
+      <Route exact path="/" component={Login} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/lobby" component={Lobby} />
+      <Route exact path="/pregame" component={Pregame} />
+      <Route exact path="/game" component={Game} />
+      <Route exact path="/postgame" component={Postgame} />
+      <Footer />
+    </div>
+  </Router>
+  );
+}
+
+export default App;
---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import GithubIcon from "../../assets/icons/Github.png";
+import FooterStyle from "./Footer.css"
+
+function Footer() {
+  return (
+    <footer className="footer-margin fixed-bottom card-footer bg-light footer-font-style text-muted footer-position" style={FooterStyle}>
+      <a href="https://github.com/carlypeyton/Project3" target="_blank" rel="noreferrer">
+        <img src={GithubIcon} alt="Github Logo" className="icon"/></a>
+    </footer>
+  );
+}
+
+export default Footer;
