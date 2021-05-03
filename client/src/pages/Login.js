2e3d5b76f0dbe9e85aaa65496008d40288d96897
Carly Peyton <carlypeyton@gmail.com>
2021-05-03 19:37:25 -0400

    Add login page.

---
+++ client/src/pages/Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Form from "../components/Form/Form.js"
+
+const Login = () => {
+  return (
+  <div className="container">
+    <div className="row">
+      <div className="col">
+        <Form />
+      </div>
+    </div>
+  </div>
+  )
+};
+
+export default Login;
