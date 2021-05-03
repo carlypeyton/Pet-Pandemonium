7a692f0e0d6be6800417e8236b579827c577b980
jacob-af <jfeitler@gmail.com>
2021-05-03 16:59:02 -0400

    fixed className error

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -7,19 +7,31 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-sm navbar-light bg-light">
       <h1 className="navbar-brand">Pet Pandemonium</h1>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation"
-        aria-expanded="false" aria-label="Toggle navigation" onClick={() => (show === "" ? setShow("show") : setShow(""))} >
-        <i class="fas fa-angle-down"></i>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navigation"
+        aria-controls="navigation"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+        onClick={() => (show === "" ? setShow("show") : setShow(""))}
+      >
+        <i className="fas fa-angle-down"></i>
       </button>
-      <div className={`collapse navbar-collapse justify-content-end ${show}`} id="navigation">
+      <div
+        className={`collapse navbar-collapse justify-content-end ${show}`}
+        id="navigation"
+      >
         <div className="nav navbar-nav ml-auto text-right">
           {/* If user is logged in, show in navbar */}
-          <a id="link-logout" class="nav-link" href="/logout">Logout</a>
-          
+          <a id="link-logout" className="nav-link" href="/logout">
+            Logout
+          </a>
         </div>
       </div>
     </nav>
-  )
+  );
 };
 
 export default Navbar;
