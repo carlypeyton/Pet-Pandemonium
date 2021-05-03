02c3bfafdd7244eee0671ba5a8664bd72f746c1b
Carly Peyton <carlypeyton@gmail.com>
2021-05-03 19:33:02 -0400

    Add footer component.

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import FooterStyle from "./Footer.css"
 function Footer() {
   return (
     <footer className="footer-margin fixed-bottom card-footer bg-light footer-font-style text-muted footer-position" style={FooterStyle}>
-      <a href="https://github.com/carlypeyton/Project3" target="_blank" rel="noreferrer">
+      <a href="https://github.com/carlypeyton/Project3" target="_blank" rel="noopener noreferrer">
         <img src={GithubIcon} alt="Github Logo" className="icon"/></a>
     </footer>
   );
