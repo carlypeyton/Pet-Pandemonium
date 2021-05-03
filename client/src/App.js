cb20cba12d0e9029977ccf11bd0d87434d690e47
Carly Peyton <carlypeyton@gmail.com>
2021-05-03 19:36:24 -0400

    Uncomment out to test rendering.

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-// import Login from "./pages/Login";
-// import Lobby from "./pages/Lobby";
-// import Pregame from "./pages/Pregame";
-// import Game from "./pages/Game";
+import Login from "./pages/Login";
+import Lobby from "./pages/Lobby";
+import Pregame from "./pages/Pregame";
+import Game from "./pages/Game";
 // import Postgame from "./pages/Postgame";
 
 function App() {
@@ -13,12 +13,12 @@ function App() {
     <Router>
       <div>
         <Navbar />
-        {/* <Route exact path="/" component={Login} />
+        <Route exact path="/game" component={Game} />
+        <Route exact path="/" component={Login} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/lobby" component={Lobby} />
         <Route exact path="/pregame" component={Pregame} />
-        <Route exact path="/game" component={Game} />
-        <Route exact path="/postgame" component={Postgame} /> */}
+        {/* <Route exact path="/postgame" component={Postgame} /> */}
         <Footer />
       </div>
     </Router>
