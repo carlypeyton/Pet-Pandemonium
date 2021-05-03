197e64eea78238167f55688a09275b7f06636ebf
jacob-af <jfeitler@gmail.com>
2021-05-03 17:00:11 -0400

    added drag and drop context

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from "react";
-import Pet from "../components/Pet";
+import { DndProvider, useDrop } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
+import Pet from "../components/Pet/Pet";
+import Tile from "../components/Tile/Tile";
+
 import field from "../utils/createField";
 import pets from "../utils/petHelper";
 
@@ -22,34 +27,33 @@ const Game = () => {
     setPlayerField(newField);
   };
 
+  const rotatePet = index => {
+    console.log(index);
+    let newPets = [...playerPets];
+    newPets[index].horizontal = !playerPets[index].horizontal;
+    setPlayerPets(newPets);
+  };
+
   return (
-    <div>
-      <div
-        style={{
-          display: "grid",
-          gridTemplateColumns: "repeat(10, 20px)",
-          gridTemplateRows: "repeat(10, 20px)"
-        }}
-      >
-        {playerField.map((tile, index) => (
-          <div
-            style={{
-              gridColumn: (index % 10) + 1,
-              gridRow: Math.floor(index / 10) + 1,
-              backgroundColor:
-                tile.hit && tile.contents ? "red" : tile.hit ? "green" : "blue",
-              border: "1px solid white"
-            }}
-            onClick={() => handleHit(index)}
-            key={index}
-          ></div>
+    <DndProvider backend={HTML5Backend}>
+      <div>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(10, 20px)",
+            gridTemplateRows: "repeat(10, 20px)"
+          }}
+        >
+          {playerField.map((tile, index) => (
+            <Tile tile={tile} key={index} index={index} handleHit={handleHit} />
+          ))}
+        </div>
+        {playerPets.map((pet, index) => (
+          <Pet data={pet} key={pet.name} index={index} rotatePet={rotatePet} />
         ))}
+        <button onClick={addContent}></button>
       </div>
-      {playerPets.map((pet, index) => (
-        <Pet data={pet} key={index} />
-      ))}
-      <button onClick={addContent}></button>
-    </div>
+    </DndProvider>
   );
 };
 
