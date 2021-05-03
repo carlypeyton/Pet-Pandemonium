ef0973583dadfe36846497ee93c6ea60d0ce0b2e
jacob-af <jfeitler@gmail.com>
2021-05-03 14:02:55 -0400

    data for cat and dog team make up

---
+++ client/src/utils/petHelper.js
@@ -0,0 +1,82 @@
+// import pet images here and attach to each object by using a variable
+
+const cats = [{
+    name: "munchkin",
+    length: 2,
+    remainingHealth: 2,
+    position: 101,
+    horizontal: true
+  },
+  {
+    name: "tuxedo",
+    length: 3,
+    remainingHealth: 3,
+    position: 102,
+    horizontal: true
+  },
+  {
+    name: "conductor",
+    length: 3,
+    remainingHealth: 3,
+    position: 103,
+    horizontal: true
+  },
+  {
+    name: "maine coon",
+    length: 4,
+    remainingHealth: 4,
+    position: 104,
+    horizontal: true
+  },
+  {
+    name: "savanna",
+    length: 5,
+    remainingHealth: 5,
+    position: 105,
+    horizontal: true
+  }
+],
+
+const dogs = [{
+    name: "yorkshire",
+    length: 2,
+    remainingHealth: 2,
+    position: 101,
+    horizontal: true
+  },
+  {
+    name: "shiba inu",
+    length: 3,
+    remainingHealth: 3,
+    position: 102,
+    horizontal: true
+  },
+  {
+    name: "corgy",
+    length: 3,
+    remainingHealth: 3,
+    position: 103,
+    horizontal: true
+  },
+  {
+    name: "toast",
+    length: 4,
+    remainingHealth: 4,
+    position: 104,
+    horizontal: true
+  },
+  {
+    name: "great dane",
+    length: 5,
+    remainingHealth: 5,
+    position: 105,
+    horizontal: true
+  }
+]
+
+const pets = {
+    cats,
+    dogs
+}
+
+export default pets;
