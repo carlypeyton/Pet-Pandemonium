0c3ac39421c682f3d1d240f8f04ca90b29caf97b
Carly Peyton <carlypeyton@gmail.com>
2021-05-03 19:33:35 -0400

    Add instructions component.

---
+++ client/src/components/Instructions/Instructions.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Instructions() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1>Instructions</h1>
+          <p>This is a classic battleship style game. You will choose
+          to compete as either cats or dogs, and will try to distract your
+          opponent to ensure that your human's attention will belong solely to you!
+      </p>
+          <br />
+          <br />
+          <p>Place your pets on the board in the pattern that you believe will
+          give you the best chance for success. Once you have placed all of your pets,
+          you and your opponent will take turns lobbing treats and toys at each other.
+      </p>
+          <br />
+          <br />
+          <p>When a pet is hit with the appropriate number of treats or toys, they will be
+          be distracted and will be off the board. Once all pets on one side are distracted, the
+          winning side with claim all of the human's attention!!!
+      </p>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+export default Instructions;
