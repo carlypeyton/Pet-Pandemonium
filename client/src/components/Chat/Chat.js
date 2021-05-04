c9da93120379b37fac635d190d8d51a39e32b539
jacob-af <jfeitler@gmail.com>
2021-05-04 01:19:25 -0400

    chat rendering properly

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,36 +1,49 @@
 import React, { useRef, useEffect, useState } from "react";
 import "./chat.css";
-import openSocket from "socket.io-client";
-const socket = openSocket("http://localhost:8000");
-
+//import io from "socket.io-client";
+//const socket = io("http://localhost:3001");
+import socket from "../../utils/socket";
+//let socket;
 
 const Chat = () => {
   const [chatLog, setChatLog] = useState([]);
+  const [user, setUser] = useState("Anon");
+  const [room, setRoom] = useState("Main");
   const inputRef = useRef();
-  const sendSocketIO = (event) => {
-    console.log(chatLog);
-    event.preventDefault();
-    socket.emit("chat_message", inputRef.current.value);
-  };
+
   useEffect(() => {
-    console.log("test");
-    socket.on("chat_message", (data) => {
-      console.log(chatLog);
-      let newChatLog = [...chatLog, data];
-      console.log(newChatLog);
-      setChatLog(newChatLog);
+    //socket = io(CONNECTION_PORT);
+    socket.emit("join_room", room);
+  }, [room]);
+
+  useEffect(() => {
+    socket.on("receive_message", data => {
+      setChatLog([...chatLog, data]);
     });
   }, [chatLog]);
+
+  const sendMessage = async event => {
+    event.preventDefault();
+    let newMessage = {
+      room,
+      user,
+      text: inputRef.current.value
+    };
+    await socket.emit("chat_message", newMessage);
+    setChatLog([...chatLog, newMessage]);
+    inputRef.current.value = "";
+  };
+
   return (
-    <div>
+    <div className="container">
       <ul id="messages">
         {chatLog.map((msg, index) => {
-          return <li key={index}>{msg}</li>;
+          return <li key={index}>{msg.user + " " + msg.text}</li>;
         })}
       </ul>
       <form id="form" action="">
-        <input id="input" autocomplete="off" ref={inputRef} />
-        <button onClick={sendSocketIO}>Send</button>
+        <input id="input" autoComplete="off" ref={inputRef} />
+        <button onClick={sendMessage}>Send</button>
       </form>
     </div>
   );
