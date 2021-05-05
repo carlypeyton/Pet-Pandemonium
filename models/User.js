de1576ca772a5571c7db700dbee465113acb6152
jacob-af <jfeitler@gmail.com>
2021-05-05 17:06:41 -0400

    updated user model

--- models/User.js
+++ models/User.js
@@ -4,7 +4,9 @@ const UserSchema = new mongoose.Schema({
   username: { type: String },
   email: { type: String, required: true, unique: true },
   password: { type: String },
-  date: { type: Date, default: Date.now }
+  date: { type: Date, default: Date.now },
+  wins: { type: Number, default: 0 },
+  losses: { type: Number, default: 0 }
 });
 
 module.exports = User = mongoose.model("users", UserSchema);
