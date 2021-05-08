e9860dba6608c62a80ba265f07fc1f341a774527
Carly Peyton <carlypeyton@gmail.com>
2021-05-08 12:31:03 -0400

    Update socket.

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,3 +1,9 @@
 import io from "socket.io-client";
-const socket = io("http://localhost:3001");
+const socketURI = ( () => {
+  if(window.location.host === 'localhost'){
+    return 'http://localhost:3001'
+  }
+  return window.location.href
+})();
+const socket = io(socketURI);
 export default socket;
