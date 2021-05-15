d0b8eb4fe21b2d3953a5d6651123d43f517a0148
jacob-af <jfeitler@gmail.com>
2021-05-15 11:53:16 -0400

    opponent moves now showing

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -5,6 +5,7 @@ import GameBoard from "../components/GameBoard/GameBoard";
 import OpponentBoard from "../components/GameBoard/OpponentBoard";
 import Pregame from "./Pregame";
 import Postgame from "./Postgame";
+import Chat from "../components/Chat/Chat";
 
 import { useGameContext } from "../utils/GameState";
 import { useChatContext } from "../utils/ChatState";
@@ -26,7 +27,7 @@ const Game = () => {
     });
     socket.on("opponent_move", data => {
       console.log("opponent_move", data);
-      if (gameState.player.field[data.index].contents !== "") {
+      if (gameState.player.field[data.index].contents !== 99) {
         gameDispatch({ type: "OPPONENT_HIT", data });
       } else {
         gameDispatch({ type: "OPPONENT_MISS", data });
@@ -48,10 +49,13 @@ const Game = () => {
   if (gameState.gamePhase === "ready" && gameState.opponentStatus === "ready") {
     return (
       <div className="row">
-        <div className="col-6">
+        <div className="col-5">
           <GameBoard />
         </div>
-        <div className="col-6">
+        <div className="col-2">
+          <Chat />
+        </div>
+        <div className="col-5">
           <OpponentBoard />
         </div>
       </div>
