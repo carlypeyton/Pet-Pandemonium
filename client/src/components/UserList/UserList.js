b77a8516fdbd25879f192004d0d39910d75aae92
jacob-af <jfeitler@gmail.com>
2021-05-15 11:53:47 -0400

    better layout for chat and userlist

---
+++ client/src/components/UserList/UserList.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+
+import UserData from "./UserData";
+import "./userlist.css";
+
+import { useChatContext } from "../../utils/ChatState";
+
+const UserList = () => {
+  const [{ users }, chatDispatch] = useChatContext();
+
+  return (
+    <div className="user-list">
+      <div className="row">
+        <div className="col" id="users">
+          {users.map(user => {
+            return <UserData user={user} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserList;
