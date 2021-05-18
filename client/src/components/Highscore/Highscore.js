fc8a1ea757cf6687c6935fb70073f91967c22c70
jacob-af <jfeitler@gmail.com>
2021-05-18 11:25:43 -0400

    highscores displayed as a table

--- client/src/components/Highscore/Highscore.js
+++ client/src/components/Highscore/Highscore.js
@@ -1,17 +1,34 @@
 import React, { useEffect, useState } from "react";
 
+import "./highscores.css";
+
+import Score from "./Score";
+
 function Highscore({ highScores }) {
   return (
-    <div className="container">
-      {highScores.map(score => {
-        return (
-          <div>
-            <p key={score._id}>
-              {score.userName} {score.wins} {score.losses}
-            </p>
-          </div>
-        );
-      })}
+    <div className="container highscores">
+      <table className="table table-bordered table-hover table-striped">
+        <thead>
+          <tr>
+            <td>User Name</td>
+            <td>Wins</td>
+            <td>Losses</td>
+            <td>Average</td>
+          </tr>
+        </thead>
+        <tbody>
+          {highScores ? (
+            highScores.map(score => <Score key={score._id} score={score} />)
+          ) : (
+            <tr>
+              <td>{"..."}</td>
+              <td>{"Loading"}</td>
+              <td>{"..."}</td>
+              <td>{"Loading"}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 }
